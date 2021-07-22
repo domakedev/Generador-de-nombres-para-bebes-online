6d63a05041d66572671430b53abb5394cf40ca8a
Cesar Guevara Cabrera <sarcansino@gmail.com>
2021-07-22 16:33:22 -0500

    termine front antes de modal

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,17 @@ module.exports = {
       'Zen': ['Zen Loop', 'ui-serif'],
     },
     extend: {
+      boxShadow: ['active'],
+
       backgroundImage: theme => ({
         'cloud': "url('assets/Nubes-de-Fondo.svg')",
         'bebe-hombre': "url('assets/bebe-hombre.svg')",
         'bebe-neutro': "url('assets/bebe-neutro.svg')",
         'bebe-mujer': "url('assets/bebe-mujer.svg')",
+        'bebe-neutro': "url('assets/bebe-neutro.svg')",
+        'peru': "url('assets/banderas/Peru.svg')",
+        'francia': "url('assets/banderas/francia.svg')",
+        'alemania': "url('assets/banderas/alemania.svg')",
        })
     },
   },
